6b0d42717bcad53ae563e141c944ab75b596146f
Tom Pacheco <tom.pacheco@comfortsystemsusa.com>
2025-03-14 15:09:20 -0400

    feat(xlsx) add WriteToSheet to write table to a specified sheet

--- xlsx.go
+++ xlsx.go
@@ -17,6 +17,8 @@ func (tbl *Table[T]) XLSXWriter() *XLSX[T] {
 	return &XLSX[T]{*tbl}
 }
 
+// WriteToFile writes the data to the given file
+// If the file already exists, it will be overwritten
 func (ct *XLSX[T]) WriteToFile(name string) error {
 
 	f := excelize.NewFile()
@@ -29,22 +31,20 @@ func (ct *XLSX[T]) WriteToFile(name string) error {
 
 	n := f.GetActiveSheetIndex()
 	sheetName := f.GetSheetName(n)
-	{
-		header := ct.HeaderNames()
-		f.SetSheetRow(sheetName, "A1", &header)
-	}
 
-	for i, row := range ct.Rows() {
-		f.SetSheetRow(sheetName, fmt.Sprintf("A%d", i+2), &row)
-	}
+	err := ct.WriteToSheet(f, sheetName)
+	if err != nil {
+		return err
 
-	err := f.SaveAs(name)
+	}
+	err = f.SaveAs(name)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+// WriteTo writes the data to the given writer (implements io.WriterTo).
 func (ct *XLSX[T]) WriteTo(w io.Writer) (int64, error) {
 	f := excelize.NewFile()
 	defer func() {
@@ -56,13 +56,26 @@ func (ct *XLSX[T]) WriteTo(w io.Writer) (int64, error) {
 
 	n := f.GetActiveSheetIndex()
 	sheetName := f.GetSheetName(n)
-	{
-		header := ct.HeaderNames()
-		f.SetSheetRow(sheetName, "A1", &header)
-	}
 
-	for i, row := range ct.Rows() {
-		f.SetSheetRow(sheetName, fmt.Sprintf("A%d", i+2), &row)
-	}
+	ct.WriteToSheet(f, sheetName)
+
 	return f.WriteTo(w)
+}
+
+// WriteToSheet writes the data to the given sheet
+// If the sheet already exists, it may be overwritten
+func (ct *XLSX[T]) WriteToSheet(f *excelize.File, name string) error {
+
+	header := ct.HeaderNames()
+	err := f.SetSheetRow(name, "A1", &header)
+	if err != nil {
+		return err
+	}
+	for i, row := range ct.Rows() {
+		err = f.SetSheetRow(name, fmt.Sprintf("A%d", i+2), &row)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
